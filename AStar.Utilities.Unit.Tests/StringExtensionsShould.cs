cea99b4b9c210093ac9f31f07950d3bc791c2fb5
JasonBarden <jason.barden@outlook.com>
2023-10-18 14:38:48 +0100

    A couple of string extensions as a simple starting point

---
+++ AStar.Utilities.Unit.Tests/StringExtensionsShould.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+
+namespace AStar.Utilities.Unit.Tests;
+
+public class StringExtensionsShould
+{
+    [Fact]
+    public void ReturnTrueForCallToIsNullWhenStringIsNull()
+    {
+        string nullString = null!;
+
+        nullString.IsNull().Should().BeTrue();
+    }
+
+    [Fact]
+    public void ReturnFalseForCallToIsNullWhenStringIsNotNull()
+    {
+        var nullString = string.Empty;
+
+        nullString.IsNull().Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReturnFalseForCallToIsNotNullWhenStringIsNull()
+    {
+        string nullString = null!;
+
+        nullString.IsNotNull().Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReturnTrueForCallToIsNotNullWhenStringIsNotNull()
+    {
+        var nullString = string.Empty;
+
+        nullString.IsNotNull().Should().BeTrue();
+    }
+}
---
+++ AStar.Utilities/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace AStar.Utilities;
+
+/// <summary>
+/// The <see cref="StringExtensions" /> class contains some useful methods to enable checks to be
+/// performed in a more fluid, English sentence, style.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// The IsNull method, as you would expect, checks whether the string is, in fact, null.
+    /// </summary>
+    /// <param name="value">The string to check for being null.</param>
+    /// <returns>True if the string is null, False otherwise.</returns>
+    public static bool IsNull(this string? value) => value is null;
+
+    /// <summary>
+    /// The IsNotNull method, as you would expect, checks whether the string is not null.
+    /// </summary>
+    /// <param name="value">The string to check for being not null.</param>
+    /// <returns>True if the string is not null, False otherwise.</returns>
+    public static bool IsNotNull(this string? value) => !value.IsNull();
+}
