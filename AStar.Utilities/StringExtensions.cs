6a3ebf87fb6dc23607a4e886654fda0bcaea382b
JasonBarden <jason.barden@outlook.com>
2023-10-21 21:01:45 +0100

    Add filter for NuGet packages and update readme

--- AStar.Utilities/StringExtensions.cs
+++ AStar.Utilities/StringExtensions.cs
@@ -7,21 +7,21 @@
 public static class StringExtensions
 {
     /// <summary>
-    /// The IsNull method, as you would expect, checks whether the string is, in fact, null.
+    /// The IsNull method, as you might expect, checks whether the string is, in fact, null.
     /// </summary>
     /// <param name="value">The string to check for being null.</param>
     /// <returns>True if the string is null, False otherwise.</returns>
     public static bool IsNull(this string? value) => value is null;
 
     /// <summary>
-    /// The IsNotNull method, as you would expect, checks whether the string is not null.
+    /// The IsNotNull method, as you might expect, checks whether the string is not null.
     /// </summary>
     /// <param name="value">The string to check for being not null.</param>
     /// <returns>True if the string is not null, False otherwise.</returns>
     public static bool IsNotNull(this string? value) => !value.IsNull();
 
     /// <summary>
-    /// The ToJson method, as you would expect, converts the object to the appropriate JSON representation.
+    /// The ToJson method, as you might expect, converts the object to the appropriate JSON representation.
     /// </summary>
     /// <param name="object">The object to convert to JSON.</param>
     /// <returns>The JSON string of the object supplied.</returns>
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # astar-utilities
-A collection of useful utilities
+A collection of useful utilities. At the moment, the collection is small but will grow as time and need permits.
+
+## Utilities
+
+### String Utilities
+
+* IsNull - as you might expect, checks whether the string is, in fact, null.
+* IsNotNull - as you might expect, checks whether the string is not null.
+* ToJson - as you might expect, converts the object to the appropriate JSON representation.
 
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=jbarden_astar-utilities&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=jbarden_astar-utilities)
